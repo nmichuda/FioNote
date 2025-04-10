162e40e00f84e328b66f4cd618ec71b0ec19568f
Michuda, Nick [JJCUS NON-J&J] <NMichuda@its.jnj.com>
2025-04-10 10:25:51 -0400

    Initial Commit

---
+++ FioNote/FioNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  FioNoteApp.swift
+//  FioNote
+//
+//  Created by Nick Michuda on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct FioNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FioNoteTests/FioNoteTests.swift
@@ -0,0 +1,16 @@
+//
+//  FioNoteTests.swift
+//  FioNoteTests
+//
+//  Created by Nick Michuda on 4/10/25.
+//
+
+import Testing
+
+struct FioNoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
